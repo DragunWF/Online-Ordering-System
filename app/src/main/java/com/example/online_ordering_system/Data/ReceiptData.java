38bb65532462f25716a95fab67d86b9abddc4f8e
papsykle <toramgarcia@gmail.com>
2024-03-22 22:02:34 +0800

    inulet

---
+++ app/src/main/java/com/example/online_ordering_system/Data/ReceiptData.java
@@ -0,0 +1,61 @@
+package com.example.online_ordering_system.Data;
+
+public class ReceiptData {
+    private int transactionID;
+    private int shippingFee;
+    private String shopName;
+    private String address;
+
+    public ReceiptData(int transactionID, int shippingFee, ShopDetails shop, CustomerDetails customer) {
+        this.shopName = shop.shopName;
+        this.address = customer.address;
+        this.transactionID = transactionID;
+        this.shippingFee = shippingFee;
+    }
+
+    public ReceiptData() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "ReceiptData{" +
+                "transactionID=" + transactionID +
+                ", shippingFee=" + shippingFee +
+                ", shopName='" + shopName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public int getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(int shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
