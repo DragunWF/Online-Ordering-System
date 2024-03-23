f9e1131d281ebab49ded36d31946b5849ce509bc
papsykle <toramgarcia@gmail.com>
2024-03-23 19:52:06 +0800

    refactored

--- app/src/main/java/com/example/online_ordering_system/data/CustomerDetails.java
+++ app/src/main/java/com/example/online_ordering_system/data/CustomerDetails.java
@@ -1,4 +1,4 @@
-package com.example.online_ordering_system.Data;
+package com.example.online_ordering_system.data;
 
 public class CustomerDetails extends Details {
     public CustomerDetails(String name, int age, String email, int mobileNumber, String address, boolean accountType) {
