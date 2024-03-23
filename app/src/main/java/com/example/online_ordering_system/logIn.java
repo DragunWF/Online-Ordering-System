a0016a4a4eac0fed9cf37073d63526839f3d503a
JasDev <jay.arnon.sinahunon12@gmail.com>
2024-03-23 12:48:00 +0800

    logIn and signUp xml&java files

---
+++ app/src/main/java/com/example/online_ordering_system/logIn.java
@@ -0,0 +1,14 @@
+package com.example.online_ordering_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
