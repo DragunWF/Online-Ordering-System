805defde6ffc20d0c86b99c2bee6efeef7873461
Marc Plarisan <plarisanmarc2@gmail.com>
2024-03-23 14:38:37 +0800

    Rename classes

--- app/src/main/java/com/example/online_ordering_system/signUp.java
+++ app/src/main/java/com/example/online_ordering_system/signUp.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class signUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
