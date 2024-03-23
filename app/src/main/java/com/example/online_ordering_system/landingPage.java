b4af52b600ed8317789613c2c7ae50066a35154b
Marc Plarisan <plarisanmarc2@gmail.com>
2024-03-23 14:41:15 +0800

    Rename class (Naming convention)

--- app/src/main/java/com/example/online_ordering_system/landingPage.java
+++ app/src/main/java/com/example/online_ordering_system/landingPage.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class landingPage extends AppCompatActivity {
+public class LandingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
