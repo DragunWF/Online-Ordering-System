c9eccc351df362089c92f3af5323951249f994c5
dragunwf <plarisanmarc2@gmail.com>
2024-03-24 16:12:35 +0800

    Update LandingPageActivity Both java and xml files

--- app/src/main/java/com/example/online_ordering_system/LandingPageActivity.java
+++ app/src/main/java/com/example/online_ordering_system/LandingPageActivity.java
@@ -2,13 +2,27 @@ package com.example.online_ordering_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LandingPageActivity extends AppCompatActivity {
+    private Button loginBtn;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+        signUpBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, SignUpActivity.class));
+        });
     }
 }
