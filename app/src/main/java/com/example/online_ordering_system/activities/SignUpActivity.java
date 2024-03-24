230871ec401dfa89d7b3e8b985299b1b71aa72e1
dragunwf <plarisanmarc2@gmail.com>
2024-03-24 19:16:05 +0800

    Update SignUpActivity.java

--- app/src/main/java/com/example/online_ordering_system/activities/SignUpActivity.java
+++ app/src/main/java/com/example/online_ordering_system/activities/SignUpActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.online_ordering_system.R;
+import com.example.online_ordering_system.utils.AccountAuthentication;
+import com.example.online_ordering_system.utils.Utils;
 
 public class SignUpActivity extends AppCompatActivity {
     private ImageButton backBtn;
@@ -17,7 +20,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText passwordText;
     private EditText fullNameText;
     private EditText emailText;
-    private EditText mobileNumText;
+    private EditText mobileNumberText;
     private EditText addressText;
 
     @Override
@@ -29,7 +32,7 @@ public class SignUpActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.passWord);
         fullNameText = findViewById(R.id.fullName);
         emailText = findViewById(R.id.email);
-        mobileNumText = findViewById(R.id.mobileNum);
+        mobileNumberText = findViewById(R.id.mobileNum);
         addressText = findViewById(R.id.address);
 
         backBtn = findViewById(R.id.backBtnSignUp);
@@ -40,6 +43,30 @@ public class SignUpActivity extends AppCompatActivity {
         });
         signUpSubmitBtn.setOnClickListener(v -> {
             // TODO: Set up Account Registration
+            String username = Utils.getString(usernameText), password = Utils.getString(passwordText),
+                    fullName = Utils.getString(fullNameText), email = Utils.getString(emailText),
+                    mobileNumber = Utils.getString(mobileNumberText), address = Utils.getString(addressText);
+            if (username.isEmpty()) {
+                errorMessage("Username is empty!");
+            } else if (AccountAuthentication.isAccountExists(username)) {
+                errorMessage("Username is already taken!");
+            } else if (password.length() < 8) {
+                errorMessage("Password must at least be equal or greater than 8 characters!");
+            } else if (email.isEmpty()) {
+                errorMessage("Email is invalid!");
+            } else if (fullName.isEmpty()) {
+                errorMessage("Full name field is empty!");
+            } else if (mobileNumber.isEmpty()) {
+                errorMessage("Mobile number is invalid!");
+            } else if (address.isEmpty()) {
+                errorMessage("Address is empty!");
+            } else {
+                // TODO: Create Account
+            }
         });
+    }
+
+    private void errorMessage(String message) {
+        Utils.displayToast(this, message);
     }
 }
