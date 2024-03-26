87bd34e5e5510c488434564c365b94af1d092798
dragunwf <plarisanmarc2@gmail.com>
2024-03-26 14:07:15 +0800

    Update Main Activity Both Java and XML Files

--- app/src/main/java/com/example/online_ordering_system/MainActivity.java
+++ app/src/main/java/com/example/online_ordering_system/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.online_ordering_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+
+import com.example.online_ordering_system.activities.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView profileView;
+    private SearchView productSearchView;
+    private Spinner productSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileView = findViewById(R.id.viewProfile);
+        productSearchView = findViewById(R.id.productSearchView);
+        productSpinner = findViewById(R.id.productSpinner);
+
+        profileView.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        });
     }
 }
