8a2d2d5b5ff441b367731851982d4145f208fcd0
dragunwf <plarisanmarc2@gmail.com>
2024-03-26 15:01:21 +0800

    Create Category.java

---
+++ app/src/main/java/com/example/online_ordering_system/data/Category.java
@@ -0,0 +1,28 @@
+package com.example.online_ordering_system.data;
+
+public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // GETTERS AND SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
