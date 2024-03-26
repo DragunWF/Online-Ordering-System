cd3cab104e33f7fc7a020034744630f3eb1ef2d4
dragunwf <plarisanmarc2@gmail.com>
2024-03-26 16:28:10 +0800

    Update SessionData.java

--- app/src/main/java/com/example/online_ordering_system/utils/SessionData.java
+++ app/src/main/java/com/example/online_ordering_system/utils/SessionData.java
@@ -1,5 +1,6 @@
 package com.example.online_ordering_system.utils;
 
+import com.example.online_ordering_system.data.Category;
 import com.example.online_ordering_system.data.Customer;
 import com.example.online_ordering_system.data.Product;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 public class SessionData {
     private static final List<Product> itemCart = new ArrayList<>();
+    private static List<Category> categories = new ArrayList<>();
     private static Customer currentUser;
 
     public static Customer getCurrentUser() {
@@ -18,7 +20,34 @@ public class SessionData {
         SessionData.currentUser = currentUser;
     }
 
+    public static Category getCategoryById(int id) {
+        for (Category category : categories) {
+            if (category.getId() == id) {
+                return category;
+            }
+        }
+        return null;
+    }
+
+    public static Category getCategoryByName(String name) {
+        for (Category category : categories) {
+            if (category.getName().equalsIgnoreCase(name)) {
+                return category;
+            }
+        }
+        return null;
+    }
+
+    // GETTERS AND SETTERS
     public static List<Product> getItemCart() {
         return itemCart;
+    }
+
+    public static List<Category> getCategories() {
+        return categories;
+    }
+
+    public static void setCategories(List<Category> categories) {
+        SessionData.categories = categories;
     }
 }
