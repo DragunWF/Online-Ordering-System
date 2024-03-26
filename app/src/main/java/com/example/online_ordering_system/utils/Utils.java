4a9d07647cd22a70983682dd87e00c0af5c1e8ba
dragunwf <plarisanmarc2@gmail.com>
2024-03-26 21:38:12 +0800

    Update Utils.java

--- app/src/main/java/com/example/online_ordering_system/utils/Utils.java
+++ app/src/main/java/com/example/online_ordering_system/utils/Utils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.online_ordering_system.data.Product;
+
 public class Utils {
     public static String getString(EditText text) {
         return String.valueOf(text.getText());
@@ -11,5 +13,21 @@ public class Utils {
 
     public static void toast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String productQuery(Product[] products) {
+        StringBuilder query = new StringBuilder();
+        query.append("VALUES ");
+        for (int i = 0 ; i < products.length; i++) {
+            Product product = products[i];
+            String row = String.format("(%s, %s, '%s', '%s', %s, %s, '%s')",
+                    product.getShopID(), product.getCategoryID(), product.getName(), product.getDescription(),
+                    product.getStock(), product.getPrice(), product.getImageURL());
+            if (i + 1 != products.length) {
+                row += ", ";
+            }
+            query.append(row);
+        }
+        return query.toString();
     }
 }
