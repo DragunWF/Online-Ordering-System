9e3a5a1715c3d19c055a87b306d62bf11a2b4d5f
papsykle <toramgarcia@gmail.com>
2024-03-28 12:44:40 +0800

    new password boilerplate

--- app/src/main/java/com/example/online_ordering_system/UpdatePassword.java
+++ app/src/main/java/com/example/online_ordering_system/UpdatePassword.java
@@ -3,12 +3,32 @@ package com.example.online_ordering_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UpdatePassword extends AppCompatActivity {
+    EditText currentPass;
+    EditText newPass;
+    EditText repeatPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_password);
+
+        currentPass = findViewById(R.id.currentPass);
+        newPass = findViewById(R.id.newPass);
+        repeatPass = findViewById(R.id.repeatPass);
+
+        currentPass.setOnFocusChangeListener((v, hasFocus) -> {
+
+        });
+
+        newPass.setOnFocusChangeListener((v, hasFocus) -> {
+
+        });
+
+        repeatPass.setOnFocusChangeListener((v, hasFocus) -> {
+
+        });
     }
 }
