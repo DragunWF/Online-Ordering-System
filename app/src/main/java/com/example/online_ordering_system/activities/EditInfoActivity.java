a5ef7d906b68f3e288f93e6de251f6db82c00449
dragunwf <plarisanmarc2@gmail.com>
2024-03-28 18:45:19 +0800

    Update EditInfoActivity.java Checkpoint

--- app/src/main/java/com/example/online_ordering_system/activities/EditInfoActivity.java
+++ app/src/main/java/com/example/online_ordering_system/activities/EditInfoActivity.java
@@ -3,14 +3,51 @@ package com.example.online_ordering_system.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.online_ordering_system.R;
+import com.example.online_ordering_system.data.Customer;
+import com.example.online_ordering_system.utils.SessionData;
+import com.example.online_ordering_system.utils.Utils;
 
 public class EditInfoActivity extends AppCompatActivity {
+    private EditText editEmailText;
+    private EditText editMobileNumberText;
+    private EditText editAddressText;
+    private Button submitBtn;
+    private Customer user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_info);
+
+        user = SessionData.getCurrentUser();
+
+        editEmailText = findViewById(R.id.editEmail);
+        editMobileNumberText = findViewById(R.id.editMobileNum);
+        editAddressText = findViewById(R.id.editAddress);
+        submitBtn = findViewById(R.id.editInfoBtn);
+
+        submitBtn.setOnClickListener(v -> {
+            String newEmail = Utils.getString(editEmailText);
+            String newMobileNumber = Utils.getString(editMobileNumberText);
+            String newAddress = Utils.getString(editAddressText);
+
+            if (newEmail.equals(user.getEmail())) {
+                toast("You are the using the same email!");
+            } else if (newMobileNumber.equals(user.getMobileNumber())) {
+                toast("You are using the same mobile number!");
+            } else if (newAddress.equals(user.getAddress())) {
+                toast("You are using the same address!");
+            } else {
+                // TODO: Implement update in the database!
+            }
+        });
+    }
+
+    private void toast(String message) {
+        Utils.toast(this, message);
     }
 }
