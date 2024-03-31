439adc6a020c6c7da592e1c819c04ee96d924d70
dragunwf <plarisanmarc2@gmail.com>
2024-03-31 19:39:44 +0800

    Bug fix - CartAdapter: Unrecognized import

--- app/src/main/java/com/example/online_ordering_system/utils/CartAdapter.java
+++ app/src/main/java/com/example/online_ordering_system/utils/CartAdapter.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
+// import com.bumptech.glide.Glide;
 import com.example.online_ordering_system.R;
 import com.example.online_ordering_system.data.Product;
 
